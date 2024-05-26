1e3ce2e7707ad6f3c40981f529bbd328f5c51f7f
Satyam Anand <satyam.anand@bolt.earth>
2024-05-27 00:46:14 +0530

    reading and manipulation done

--- app/middleware.py
+++ app/middleware.py
@@ -4,14 +4,14 @@ from flask import request
 def setup_middlewares(app, logger):
     CORS(app)
     
-    # @app.before_request
-    # def log_request_info():
-    #     logger.info('Accessed URL: %s', request.url)
-    #     logger.info('Request Headers: %s', request.headers)
-    #     logger.info('Request Body: %s', request.get_data())
+    @app.before_request
+    def log_request_info():
+        logger.info('Accessed URL: %s', request.url)
+        logger.info('Request Headers: %s', request.headers)
+        logger.info('Request Body: %s', request.get_data())
 
-    # @app.after_request
-    # def log_response_info(response):
-    #     logger.info('Response Headers: %s', response.headers)
-    #     logger.info('Response Body: %s', f'{response.data[:200]}...')
-    #     return response
+    @app.after_request
+    def log_response_info(response):
+        logger.info('Response Headers: %s', response.headers)
+        logger.info('Response Body: %s', f'{response.data[:200]}...')
+        return response
